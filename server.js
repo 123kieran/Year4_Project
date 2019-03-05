8be3991c88d7d88f6fc02a829a8d2189525a5e5b
123kieran <123kieran@gmail.com>
2019-03-05 14:05:28 +0000

    heroku test

--- server.js
+++ server.js
@@ -1,96 +1,108 @@
-var express  = require('express');
-var app      = express(); // This creates our app with express, this helps it get hosted online
-var morgan = require('morgan'); // This imports morgan to our server which logs requests to the console.
-var bodyParser = require('body-parser');    //This imports body-parser which pulls information from HTML POST 
-var methodOverride = require('method-override'); //This importes method-override into our project to  simulate DELETE and PUT
-var cors = require('cors'); // This imports cors into our project
+//server.js (todo-ionic2-heroku/server.js)
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
 
-// Our MongoDB dabase is imported to our node serve here.
-var mongoose = require('mongoose'),
-    mongoClient = mongoose.mongoClient,
-    ObjectID = mongoose.ObjectID,
-    db;
- 
-// Configuration
-// Below was for our originall local connection
-//mongoose.connect('mongodb://localhost/reviews');
+var mongodb = require('mongodb'),
+mongoClient = mongodb.MongoClient,
+ObjectID = mongodb.ObjectID, // Used in API endpoints
+db; // We'll initialize connection below
 
-//Below we connect our mLabs database to our node server
-mongoose.connect('mongodb://heroku_lz2bt2wt:kabs1g4ubvtolmbjkvold9inkd@ds227185.mlab.com:27185/heroku_lz2bt2wt');
- 
-app.use(morgan('dev')); // This log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));  // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json for sending between the app and the database
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json for sending and retrieving data from the app
-app.use(methodOverride());
-//app.set('port', process.env.PORT || 8080); // This is for connecting the app to heroku
-app.use(cors());
-app.use(express.static("www")); // Our Ionic app build is in the www folder and is got by the server so it can run on heroku.
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 8080);
+app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://heroku_lz2bt2wt:kabs1g4ubvtolmbjkvold9inkd@ds227185.mlab.com:27185/heroku_lz2bt2wt';
 
-// Tells the app which port to listen on. 
-app.listen(8080);
-console.log("App listening on port 8080");
- 
-// Model for the reviews in our app
-var Job = mongoose.model('Job', {
-    title: String,
-    description: String,
-   // rating: Number
-});
- 
-// Routes
- 
-// Get reviews
-app.get('/api/jobs', function(req, res) {
 
-    console.log("fetching jobs");
+// Initialize database connection and then start the server.
+mongoClient.connect(MONGODB_URI, function (err, database) {
+if (err) {
+process.exit(1);
+}
 
-    // use mongoose to get all reviews in the database
-    Job.find(function(err, jobs) {
+db = database; // Our database object from mLab
 
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err)
-            res.send(err)
+console.log("Database connection ready");
 
-        res.json(jobs); // return all reviews in JSON format
+
+// GET: retrieve all job
+app.get("/api/job", function(req, res) {
+    db.collection("job").find({}).toArray(function(err, docs) {
+    if (err) {
+    handleError(res, err.message, "Failed to get job");
+    } else {
+    res.status(200).json(docs);
+    }
     });
-});
-
-// create review and send back all reviews after creation
-app.post('/api/jobs', function(req, res) {
-
-    console.log("creating job");
-
-    // create a review, information comes from request from Ionic
-    Job.create({
-        title : req.body.title,
-        description : req.body.description,
-        //rating: req.body.rating,
-        done : false
-    }, function(err, job) {
-        if (err)
-            res.send(err);
-
-        // get and return all the reviews after another is created
-        Job.find(function(err, jobs) {
-            if (err)
-                res.send(err)
-            res.json(jobs);
-        });
     });
-});
-
-// delete a review
-app.delete('/api/jobs/:job_id', function(req, res) {
-    Job.remove({
-        _id : req.params.job_id
-    }, function(err, job) {
+    
+    // POST: create a new todo
+    app.post("/api/job", function(req, res) {
+    var newJob = {
+    description: req.body.description,
+    isComplete: false
+    }
+    
+    db.collection("job").insertOne(newJob, function(err, doc) {
+    if (err) {
+    handleError(res, err.message, "Failed to add job");
+    } else {
+    res.status(201).json(doc.ops[0]);
+    }
     });
+    });
+    
+    /*
+    * Endpoint "/api/job/:id"
+    */
+    
+    // GET: retrieve a todo by id -- Note, not used on front-end
+    app.get("/api/job/:id", function(req, res) {
+    db.collection("job").findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+    handleError(res, err.message, "Failed to get job by _id");
+    } else {
+    res.status(200).json(doc);
+    }
+    });
+    });
+    
+    // PUT: update a job by id
+    app.put("/api/job/:id", function(req, res) {
+    var updateTodo = req.body;
+    delete updateTodo._id;
+    
+    db.collection("job").updateOne({_id: new ObjectID(req.params.id)}, updateTodo, function(err, doc) {
+    if (err) {
+    handleError(res, err.message, "Failed to update job");
+    } else {
+    res.status(204).end();
+    }
+    });
+    });
+    
+    // DELETE: delete a todo by id
+    app.delete("/api/job/:id", function(req, res) {
+    db.collection("job").deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+    handleError(res, err.message, "Failed to delete job");
+    } else {
+    res.status(204).end();
+    }
+    });
+    });
+    
+    // Error handler for the api
+    function handleError(res, reason, message, code) {
+    console.log("API Error: " + reason);
+    res.status(code || 500).json({"Error": message});
+    }
+
+// Initialize the app.
+app.listen(app.get('port'), function () {
+console.log("Server now connected on port", app.get('port'));
+});
 });
