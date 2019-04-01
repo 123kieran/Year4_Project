bb52abdd71d6c41bcd4a00a91a1dd407fb8d08b8
123kieran <123kieran@gmail.com>
2019-04-01 14:37:34 +0100

    Working on sending messages, slight problem with getting email

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -7,6 +7,7 @@ import firebase from 'firebase'; // for password reset
 
 
 import { HomePage } from '../home/home';
+import { MessagePage } from '../message/message';
 
 
 
@@ -41,13 +42,16 @@ export class LogIn {
   public loginhome() {
     let email = this.user.email; // example@email.com, original email address
     this.showLoading();
+    this.navCtrl.push(MessagePage,{email});
     this.auth.login(this.user, {
       provider: AuthProviders.Password,
         method: AuthMethods.Password 
     }).then((authData) => {
       this.loader.dismiss();
      this.navCtrl.setRoot(HomePage);
+     this.navCtrl.push(HomePage);  //push the email to tv page
      this.navCtrl.push(HomePage,{email});  //push the email to tv page
+
     }).catch((error) => {
       this.showError(error); // if log in is unsuccessful show error
     });
--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {  NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { LogIn } from '../login/login';
 
+ 
+//@IonicPage()
 @Component({
   selector: 'page-message',
-  templateUrl: 'message.html'
+  templateUrl: 'message.html',
 })
 export class MessagePage {
+  //variables
+  email : any;
+  message : string = '';
+  time:any;
+  s;
+  _chatSubscription;
+  messages:string[];
+  
+  constructor(public db:AngularFireDatabase,public navCtrl: NavController, public navParams: NavParams){
+    this.email = this.navParams.get('email');
+  this._chatSubscription = this.db.list('/message').subscribe(data =>{
+    this.messages=data;
+    });
+}
 
-  constructor(public navCtrl: NavController) {
 
+sendMessage() {
+//Variables
+  var now = new Date();
+  var hrs = now.getHours();
+  var mins = now.getMinutes();
+//adding AM or PM
+ if(hrs<12){
+      if(mins<10 || mins == 0){
+        this.time = hrs+":"+"0"+mins+" "+"AM";          
+      }
+      else{
+        this.time = hrs+":"+mins+" "+"AM";     
+      }
   }
-
+  if(hrs == 12){
+      if(mins<10 || mins == 0){
+        this.time = hrs+":"+"0"+mins+" "+"PM";          
+      }
+      else{
+        this.time = hrs+":"+mins+" "+"PM";     
+      }
+  }
+  if(hrs>12){
+      hrs=hrs-12;
+      if(mins<10 || mins == 0){
+           this.time = hrs+":"+"0"+mins+" "+"PM";          
+      }
+      else{
+           this.time = hrs+":"+mins+" "+"PM";     
+      }
+      //pushing to database
+     this.db.list('/message').push({
+      email: this.email,
+       message: this.message,
+       time: this.time
+     }).then( () =>{
+      //message  is sent
+     }).catch( () =>{
+      //error
+    });
+    this.message='';
+   }
+  }
 }
